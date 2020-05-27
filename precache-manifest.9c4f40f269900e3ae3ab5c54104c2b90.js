704a21f5f930fc47aa1d6aac25d2d98d1e465b0f
Sebastão Giacheto Ferreira Júnior <sebastorama@gmail.com>
2020-05-27 14:47:13 -0400

    Updates

---
+++ precache-manifest.9c4f40f269900e3ae3ab5c54104c2b90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36be197e207754653b82751ce4a1724b",
+    "url": "/adubolator/index.html"
+  },
+  {
+    "revision": "9268b8730b98255a4669",
+    "url": "/adubolator/static/css/2.c4f291ab.chunk.css"
+  },
+  {
+    "revision": "d9eefcb77102d1d63e22",
+    "url": "/adubolator/static/css/main.79fa755c.chunk.css"
+  },
+  {
+    "revision": "9268b8730b98255a4669",
+    "url": "/adubolator/static/js/2.0a938349.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adubolator/static/js/2.0a938349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9eefcb77102d1d63e22",
+    "url": "/adubolator/static/js/main.e2936a45.chunk.js"
+  },
+  {
+    "revision": "a6eae36f799602258adf",
+    "url": "/adubolator/static/js/runtime-main.256fe669.js"
+  }
+]);
